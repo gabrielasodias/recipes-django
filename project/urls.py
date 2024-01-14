5e40944fd900904376abe0c944df4c002dc20c4d
Gabriela Dias <gabrieladiasofc@gmail.com>
2024-01-14 15:31:35 -0300

    Move the code to the Recipes app

--- project/urls.py
+++ project/urls.py
@@ -15,21 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse('HOME')
-
-def sobre(request):
-    return HttpResponse('SOBRE')
-
-def contato(request):
-    return HttpResponse('CONTATO')
-
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sobre/',sobre),
-    path('contato/',contato),
+    path('',include('recipes.urls'))
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+#from django.shortcuts import render
+
+def home(request):
+    return HttpResponse('HOME')
+
+def contato(request):
+    return HttpResponse('CONTATO')
+
+def sobre(request):
+    return HttpResponse('SOBRE')
