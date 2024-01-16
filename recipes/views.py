039988f499962a8715afe7ccef3c96cfb452efc7
Gabriela Dias <gabrieladiasofc@gmail.com>
2024-01-16 16:03:32 -0300

    Templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,8 @@
 from django.http import HttpResponse
-
-#from django.shortcuts import render
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('HOME')
+    return render(request, 'recipes/home.html')
 
 def contato(request):
     return HttpResponse('CONTATO')
