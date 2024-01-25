2d65ec242f99a1355881ce6dcddea4d4b7b793c3
Gabriela Dias <gabrieladiasofc@gmail.com>
2024-01-25 10:38:12 -0300

    Remove unnecessary lines

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,4 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/home.html')
-
-def contato(request):
-    return render(request, 'recipes/contact.html')
-
-def sobre(request):
-    return HttpResponse('SOBRE')
